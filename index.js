c851f422cf7320aec1909694ffe02a661ce836a4
aKoolKev <kevindong0212@gmail.com>
2024-08-02 16:51:13 -0500

    Fix subtotal bug, duplicate order in shopping cart bug, and can display quanity in shopping cart.

--- index.js
+++ index.js
@@ -82,145 +82,12 @@ function loadAppetizers(){
         //add order button
         const add_buttonEl = document.createElement('button'); //make button
         add_buttonEl.innerText = "ADD"; //button name
-        add_buttonEl.addEventListener('click', ()=>{addItemToShoppingCart(appetizerNames[i],appetizerPrices[i])});
+        add_buttonEl.addEventListener('click', ()=>{addItemToShoppingCart(appetizerNames[i],appetizerNames[i],appetizerPrices[i])});
     
         const addButton_liEl = document.createElement('li');
         addButton_liEl.appendChild(add_buttonEl);
         appetizerAddButtonContainer.appendChild(addButton_liEl);
     }
-}
-
-//print shopping cart when an item is added
-function displayShoppingCart(){
-    //show Order object in shopping cart
-    shoppingCartContentContainer.innerHTML = ''; //clear anything in the cart before
-
-    for(const order of shoppingCart){
-        const order_liEl = document.createElement('li');
-
-        // remove button
-        const remove_buttonEl = document.createElement('button');
-        remove_buttonEl.innerHTML = "X";
-        remove_buttonEl.className = "remove-item-button";
-        remove_buttonEl.value = order.name;
-        remove_buttonEl.addEventListener('click', removeItemFromShoppingCart);
-
-        // display item name and quanity
-        order_liEl.append(remove_buttonEl, document.createTextNode(order.name), document.createTextNode(' [' + order.quanity + ']'));
-
-        shoppingCartContentContainer.appendChild(order_liEl);
-    }
-
-    //compute the cost in the shopping cart
-    const displaySubtotal = document.getElementById("display-subtotal");
-    const displayGrandTotal = document.getElementById("display-grand-total");
-    displaySubtotal.innerHTML = getSubTotal().toFixed(2);
-    displayGrandTotal.innerHTML = getGrandTotal(getSubTotal()).toFixed(2);
-
-}
-
-//adds an item to the shopping cart
-function addItemToShoppingCart(itemName, itemPrice, size){
-
-    let itemNameID = itemName;
-    
-    if(size){
-        itemNameID +='-' + size; // order with different sizes have the size in their name
-    }
-
-    let itemQuanityContainer = document.getElementById(itemNameID);
-    
-    //first check if item already exists in the shopping cart
-    if (shoppingCart.findIndex(order => order.name === itemName) != -1){
-        let orderObj = shoppingCart.find(order=>order.name ===itemName);
-        let prevQuanity = orderObj.quanity;
-        orderObj.quanity = prevQuanity + parseInt(itemQuanityContainer.innerText);
-        itemQuanityContainer.innerText = 0; //reset quanity selector
-        displayShoppingCart();
-        return; //no need to make new object, just update the quanity
-    }    
-
-    // new item in shopping cart...create Order object
-    const order = {
-        name: itemName,
-        price: itemPrice,
-        quanity: parseInt(itemQuanityContainer.innerText),
-        size: size
-    }
-
-    //only add item with a valid quanity
-    if(order.quanity>0){
-        //add Order object to shopping cart
-        shoppingCart.push(order);
-
-        //reset the quanity selector to 0 after adding item to cart
-        itemQuanityContainer.innerText = 0;
-
-        //display added item in shopping cart
-        displayShoppingCart();
-    }
-    
-}
-
-//remove an item from the shopping cart
-function removeItemFromShoppingCart(e){
-
-    //item to remove
-    const itemToRemove = e.target.value;
-
-    //remove the item
-    let filteredCart = shoppingCart.filter(item =>item.name !=itemToRemove);
-    shoppingCart = filteredCart;
-
-    //display the shopping cart with the item removed
-    displayShoppingCart();
-}
-
-//return subtotal of items in the shopping cart
-function getSubTotal(){
-    let subtotal = 0;
-    
-    for(const item of shoppingCart){
-        subtotal += item.price;
-    }
-
-    return subtotal;
-}
-
-//compute and display grand total
-function getGrandTotal(subtotal){
-    const salesTax = 0.08475;
-    return  ( (subtotal*salesTax) + subtotal) ;
-}
-
-//clear all orders in the shopping cart
-function clearCart(){
-    shoppingCart.length=0; //empty cart
-    displayShoppingCart(); //show empty cart
-}
-
-
-function submitOrder() {
-    //insure there exists orders to submit
-    if (shoppingCart.length === 0) {
-        alert('Shopping Cart is empty!');
-        return;
-    }
-
-    //orders -> {order, order, ...}
-    const newOrderRef = database.ref('ORDERS').push();
-    
-    newOrderRef.set({ order: shoppingCart })
-        .then(() => {
-            console.log('Order submitted successfully!');
-            shoppingCart = [];  // Clear the selection after submission
-            displayShoppingCart(); //display the empty shopping cart
-            alert('Order submitted successfully!');
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('There was an error submitting your order.');
-        });
 }
 
 const soupNames = ['Wonton Soup', 'Egg Drop Soup', 'Wonton Egg Drop Soup', 'Hot & Sour Soup'];
@@ -300,7 +167,7 @@ function loadSoups(){
         //add small soup button
         const addSmallSoup_buttonEl = document.createElement('button'); //make button
         addSmallSoup_buttonEl.innerText = "ADD"; //button name
-        addSmallSoup_buttonEl.addEventListener('click', ()=>{addItemToShoppingCart(soupNames[i],soupSmallPrices[i],'small')});
+        addSmallSoup_buttonEl.addEventListener('click', ()=>{addItemToShoppingCart(soupNames[i] + '-small',soupNames[i],soupSmallPrices[i],'small')});
     
         const addSmallSoupButton_liEl = document.createElement('li');
         addSmallSoupButton_liEl.appendChild(addSmallSoup_buttonEl);
@@ -364,15 +231,145 @@ function loadSoups(){
         //add small soup button
         const addLargeSoup_buttonEl = document.createElement('button'); //make button
         addLargeSoup_buttonEl.innerText = "ADD"; //button name
-        addLargeSoup_buttonEl.addEventListener('click', ()=>{addItemToShoppingCart(soupNames[i],soupLargePrices[i],'large')});
+        addLargeSoup_buttonEl.addEventListener('click', ()=>{addItemToShoppingCart(soupNames[i] + '-large',soupNames[i],soupLargePrices[i],'large')});
     
         const addLargeSoupButton_liEl = document.createElement('li');
         addLargeSoupButton_liEl.appendChild(addLargeSoup_buttonEl);
         largeSoupAddButtonContainer.appendChild(addLargeSoupButton_liEl);
 
     }
+}
 
+//print shopping cart when an item is added
+function displayShoppingCart(){
+    //show Order object in shopping cart
+    shoppingCartContentContainer.innerHTML = ''; //clear anything in the cart before
+
+    for(const order of shoppingCart){
+        const order_liEl = document.createElement('li');
+
+        // remove button
+        const remove_buttonEl = document.createElement('button');
+        remove_buttonEl.innerHTML = "X";
+        remove_buttonEl.className = "remove-item-button";
+        remove_buttonEl.value = order.id;
+        remove_buttonEl.addEventListener('click', removeItemFromShoppingCart);
+
+        // display item name, quanity, and size
+        order_liEl.append(remove_buttonEl, document.createTextNode(order.name), document.createTextNode(' [' + order.quanity + ']'));
+        
+        if (order.size != '')
+            order_liEl.append(document.createTextNode(' (' + (order.size).toUpperCase() + ')'));
+        shoppingCartContentContainer.appendChild(order_liEl);
+    }
+}
+
+//adds an item to the shopping cart
+function addItemToShoppingCart(itemID, itemName, itemPrice, size){
+
+    let itemQuanityContainer = document.getElementById(itemID);
     
+    //first check if item already exists in the shopping cart
+    if (shoppingCart.findIndex(order => order.id === itemID) != -1){
+        let orderObj = shoppingCart.find(order => order.id === itemID);
+        let prevQuanity = orderObj.quanity;
+        orderObj.quanity = prevQuanity + parseInt(itemQuanityContainer.innerText);
+        itemQuanityContainer.innerText = 0; //reset quanity selector
+        displayShoppingCart();
+        getGrandTotal(getSubTotal());
+        return; //no need to make new object, just update the quanity
+    }    
+
+    // new item in shopping cart...create Order object
+    const order = {
+        id: itemID,
+        name: itemName,
+        price: itemPrice,
+        quanity: parseInt(itemQuanityContainer.innerText),
+        size: size ? size : '' //if no size, empty string, else the size of the order
+    }
+
+    //only add item with a valid quanity
+    if(order.quanity>0){
+        //add Order object to shopping cart
+        shoppingCart.push(order);
+
+        //reset the quanity selector to 0 after adding item to cart
+        itemQuanityContainer.innerText = 0;
+
+        //display added item in shopping cart
+        displayShoppingCart();
+
+        //get new price of order in cart
+        getGrandTotal(getSubTotal());
+    }
+}
+
+//remove an item from the shopping cart
+function removeItemFromShoppingCart(e){
+
+    //item to remove
+    const itemToRemove = e.target.value;
+
+    //remove the item
+    let filteredCart = shoppingCart.filter(item => item.id != itemToRemove);
+    shoppingCart = filteredCart;
+
+    //display the shopping cart with the item removed
+    displayShoppingCart();
+}
+
+//displays and returns the subtotal of items in the shopping cart
+function getSubTotal(){
+    //compute the cost in the shopping cart
+    const displaySubtotal = document.getElementById("display-subtotal");
+   
+    let subtotal = 0;
+    
+    for(const item of shoppingCart){
+        subtotal += item.price * item.quanity;
+    }
+
+    displaySubtotal.innerHTML = subtotal.toFixed(2);
+    return(subtotal);
+}
+
+//compute and display grand total
+function getGrandTotal(subtotal){
+    const displayGrandTotal = document.getElementById("display-grand-total");
+
+    const salesTax = 0.08475;
+    displayGrandTotal.innerHTML = ( (subtotal*salesTax) + subtotal).toFixed(2) ;
+}
+
+//clear all orders in the shopping cart
+function clearCart(){
+    shoppingCart.length=0; //empty cart
+    displayShoppingCart(); //show empty cart
+}
+
+
+function submitOrder() {
+    //insure there exists orders to submit
+    if (shoppingCart.length === 0) {
+        alert('Shopping Cart is empty!');
+        return;
+    }
+
+    //orders -> {order, order, ...}
+    const newOrderRef = database.ref('ORDERS').push();
+    
+    newOrderRef.set({ order: shoppingCart })
+        .then(() => {
+            console.log('Order submitted successfully!');
+            shoppingCart = [];  // Clear the selection after submission
+            displayShoppingCart(); //display the empty shopping cart
+            alert('Order submitted successfully!');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('There was an error submitting your order.');
+        });
 }
 
 window.onload = function(){
